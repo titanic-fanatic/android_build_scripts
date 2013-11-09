1fbb669c5a7a00e70a6204a59dc591605e2d8fc9
Tyler Janzen <tylor.janzen@gmail.com>
2013-11-08 23:40:33 -0700

    Updated Build Script
    Revamped entire script and added support for command line arguments. Use -h (--help) for usage instructions.

--- start_build.sh
+++ start_build.sh
@@ -1,34 +1,101 @@
-#!/system/bin/sh
-
 lcyan='\e[1;32m'
+lred='\e[1;31m'
 NC='\e[0m'
 
 SCRIPTVERSION='1.0'
 
-echo " "
-echo " "
-echo -e "${lcyan}***************************************************${NC}"
-echo "                                                   "
-echo -e "               ${lcyan}CM-11.0 BUILD SCRIPT${NC}                "
-echo -e "                    ${lcyan}Version $SCRIPTVERSION${NC}         "
-echo -e "                ${lcyan}for the SGH-I757M${NC}                  "
-echo "                                                   "
-echo -e "${lcyan}***************************************************${NC}"
+### FUNCTIONS ###
+
+function banner(){
+    echo " "
+    echo " "
+    echo -e "${lcyan}***************************************************${NC}"
+    echo "                                                   "
+    echo -e "               ${lcyan}CM-11.0 BUILD SCRIPT${NC}                "
+    echo -e "                    ${lcyan}Version $SCRIPTVERSION${NC}         "
+    echo -e "                ${lcyan}for the SGH-I757M${NC}                  "
+    echo "                                                   "
+    echo -e "${lcyan}***************************************************${NC}"
+}
+
+### END FUNCTIONS ###
+
+CLOBBER='N'
+SYNC='N'
+PREBUILTS='N'
+if [ $# -eq 1 ];
+then
+    case $1 in
+        '-c')
+            CLOBBER='Y'
+        ;;
+        '-s')
+            SYNC='Y'
+        ;;
+        '-p')
+            PREBUILTS='Y'
+        ;;
+        '-cs' | '-sc')
+            CLOBBER='Y'
+            SYNC='Y'
+        ;;
+        '-cp' | '-pc')
+            CLOBBER='Y'
+            PREBUILTS='Y'
+        ;;
+        '-csp' | '-cps' | '-scp' | '-spc' | '-pcs' | '-psc')
+            CLOBBER='Y'
+            SYNC='Y'
+            PREBUILTS='Y'
+        ;;
+        '-h' | '--help')
+            banner
+            echo -e "${lcyan}                       USAGE                       ${NC}"
+            echo -e "${lcyan}***************************************************${NC}"
+            echo " "
+            echo "./start_build.sh [OPTION(s)]"
+            echo " "
+            echo "OPTIONS"
+            echo "    -c     Clobber the out directory before building"
+            echo "    -s     Sync repos before building"
+            echo "    -p     Sync pre-builts before building"
+            echo " "
+            echo -e "${lcyan}***************************************************${NC}"
+            echo " "
+            echo " "
+            
+            exit 0
+        ;;
+        *)
+            echo " "
+            echo " "
+            echo "Invalid parameter: $1"
+            echo " "
+            echo " "
+            
+            exit 1
+        ;;
+    esac
+elif [ $# -gt 0 ];
+then
+    echo " "
+    echo " "
+    echo "Too many parameters..."
+    echo " "
+    echo " "
+    
+    exit 1
+fi
+
+banner
 echo " "
 echo " "
 
 . build/envsetup.sh
 lunch cm_celoxhd-eng
 
-echo " "
-echo " "
-echo -e "${lcyan}Do you want to clobber the out directory? [Y/N]:${NC}"
-read CLOBBER
-
 if [ $CLOBBER == "Y" ];
 then
-    echo " "
-    echo " "
     echo -e "${lcyan}Clobbering out directory...${NC}"
     echo " "
     echo " "
@@ -36,6 +103,34 @@ then
     echo " "
     echo " "
     echo -e "${lcyan}Out directory clobbered!${NC}"
+    echo " "
+    echo " "
+fi
+
+if [ $SYNC == "Y" ];
+then
+    echo -e "${lcyan}Syncing repositories before build...${NC}"
+    echo " "
+    echo " "
+    repo sync -j5
+    echo " "
+    echo " "
+    echo -e "${lcyan}Syncing repositories completed!${NC}"
+    echo " "
+    echo " "
+fi
+
+if [ $PREBUILTS == "Y" ];
+then
+    echo -e "${lcyan}Syncing CM pre-builts...${NC}"
+    echo " "
+    echo " "
+    vendor/cm/get-prebuilts
+    echo " "
+    echo " "
+    echo -e "${lcyan}Finished syncing CM pre-builts!${NC}"
+    echo " "
+    echo " "
 fi
 
 
@@ -43,21 +138,28 @@ DATE_START=$(date +%m/%d/%Y-%H:%M)
 DATE_NOW=$(date +%Y%m%d-%H%M)
 start_time=$(date +%s)
 
-echo " "
-echo " "
 echo -e "${lcyan}Starting build...${NC}"
 echo -e "${lcyan}Build started at${NC} $DATE_START"
+echo -e "${lcyan}Build errors being recorded to: ${NC}logs/CM110BuildError-$DATE_NOW.log"
 echo " "
 echo " "
 
 make bacon -j24 2> logs/CM110BuildErrors-$DATE_NOW.log
 
+BUILDSTATUS=$?
 finish_time=$(date +%s)
 elapsed=$((finish_time - start_time))
 hours=$((elapsed / 3600))
 minutes=$(((elapsed - (hours * 3600)) / 60))
 seconds=$((elapsed - ((minutes * 60) + (hours *3600))))
 DATE_END=$(date +%m/%d/%Y-%H:%M)
+
+if [ $BUILDSTATUS -gt 0 ];
+then
+    echo " "
+    echo " "
+    echo -e "${lred}BUILD FAILED!${NC}"
+fi
 
 echo " "
 echo " "
